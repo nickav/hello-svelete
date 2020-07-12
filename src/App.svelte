051843e1250d72145b4cc3f96f6c4d88129fc7d8
Nick Aversano <nick23hi@gmail.com>
2020-07-12 15:57:03 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<style>
+  .time {
+    font-weight: bold;
+  }
+</style>
+
+<script>
+  import { onMount } from 'svelte';
+
+  export let name = 'Anonymous';
+  let time = new Date();
+
+  onMount(() => {
+    const timer = setInterval(() => {
+      time = new Date();
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    }
+  })
+
+  let hours, minutes, seconds;
+
+  $: {
+    hours = time.getHours();
+    minutes = time.getMinutes();
+    seconds = time.getSeconds();
+  }
+</script>
+
+<p>
+  Hello {name}, the time is <span class="time">{hours}:{minutes}:{seconds}</span>
+</p>
+
